a4f3942a8138e2497ad2f0fbfb667cb3eea922e4
Alexis capdepon <capdepon.alexis@gmail.com>
2018-11-06 11:59:48 +0100

    Update Exo9/exo9.js

--- Exo9/exo9.js
+++ Exo9/exo9.js
@@ -1,2 +1,11 @@
 'use stric'
-alert('ok');
+function checkPalindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  if (reverseStr === lowRegStr){
+  	return true
+  }
+}
+console.log(checkPalindrome("engage le jeu que je le gagne"));
+console.log(checkPalindrome('kayak'));
