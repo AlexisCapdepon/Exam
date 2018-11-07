1b74eb9991419d37254688f46c211cd13d30973f
Alexis capdepon <capdepon.alexis@gmail.com>
2018-11-07 16:44:35 +0100

    Update Exo5/exo5.js

--- Exo5/exo5.js
+++ Exo5/exo5.js
@@ -5,22 +5,16 @@ var firstName = document.querySelector("input[name='firstName']");
 var lastName = document.querySelector("input[name='lastName']");
 var email = document.querySelector("input[name='email']");
 var passWord = document.querySelector("input[name='passWord']");
-var regName = /^([a-z]+[,.]?[ ]?|[a-z]+['-]?)+$/g;
+var regfirstName = /^[A-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/g
 var regEmail = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi
 var regPassWord = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
 
-if (regName.test(firstName.value) && firstName.value != ''){
+if (regfirstName.test(firstName.value) && firstName.value != ''){
   
   firstName.style.backgroundColor = 'green';
 }else{
   firstName.value='';
   firstName.style.backgroundColor = 'red';
-}
-if (alert(regName.test(lastName.value)) && lastName.value != ''){
-  lastName.style.backgroundColor = 'green';
-}else{
-  lastName.value='';
-  lastName.style.backgroundColor = 'red';
 }
 if (regEmail.test(email.value) && email.value != ''){
   email.style.backgroundColor = 'green';
@@ -33,5 +27,11 @@ if (regPassWord.test(passWord.value) && passWord.value != ''){
 }else{
   passWord.value='';
   passWord.style.backgroundColor = 'red';
+}
+if(regfirstName.test(lastName.value) ){
+  lastName.style.backgroundColor = 'green';
+}else{
+  lastName.value='';
+  lastName.style.backgroundColor = 'red';
 }
 });
